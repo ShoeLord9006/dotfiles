daa62bd1c4d211c4e1a2af5eb5101aa9b475c172
Shoe_Eater <blakeissmelly@gmail.com>
2022-01-20 23:06:00 +0800

    XDGCONFIG stuff fucked

--- install.sh
+++ install.sh
@@ -2,12 +2,19 @@
 
 mkdir $HOME/.ssh
 
+mkdir -p $HOME/.bash
+mkdir -p $HOME/.vim
+mkdir -p ~/.config/mako
+mkdir -p ~/.config/sway
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/kitty
+
 cp bash/.bashrc $HOME/
 cp -r bash/.bash $HOME/
 cp vim/.vimrc $HOME/
 cp -r vim/.vim $HOME/
-cp -r mako $XDG_CONFIG_HOME/
-cp -r sway $XDG_CONFIG_HOME/
-cp -r waybar $XDG_CONFIG_HOME/
-cp -r kitty $XDG_CONFIG_HOME/
+cp -r mako ~/.config/
+cp -r sway ~/.config/
+cp -r waybar ~/.config/
+cp -r kitty ~/.config/
 cp ssh/id_rsa.pub $HOME/.ssh/

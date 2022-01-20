fba5e410aaed02cb433ba7159f1f80dfd42e5de9
Shoe_Eater <blakeissmelly@gmail.com>
2022-01-20 21:38:05 +0800

    stuffs happened

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir $HOME/.ssh
+
+cp bash/.bashrc $HOME/
+cp -r bash/.bash $HOME/
+cp vim/.vimrc $HOME/
+cp -r vim/.vim $HOME/
+cp -r mako $XDG_CONFIG_HOME/
+cp -r sway $XDG_CONFIG_HOME/
+cp -r waybar $XDG_CONFIG_HOME/
+cp ssh/id_rsa.pub $HOME/.ssh/
